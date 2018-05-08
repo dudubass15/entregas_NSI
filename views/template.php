5c603aa0d74b3ee50de0bf649cf1d7c87c0c80a9
Carlos Eduardo <carlos.contato00@gmail.com>
2018-05-07 23:25:52 -0300

    Retirado botão

--- views/template.php
+++ views/template.php
@@ -24,7 +24,7 @@
 				<input class="form-control form-control-dark w-100" type="text" placeholder="Buscar" aria-label="Search">
 				<ul class="navbar-nav px-3">
 					<li class="nav-item text-nowrap">
-						<a class="nav-link" href="#">Sair</a>
+						
 					</li>
 				</ul>
 			</nav>
