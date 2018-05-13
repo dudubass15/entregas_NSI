5408efca59be5df179606f05b324d7f36e3c2bf0
Carlos Eduardo <carlos.contato00@gmail.com>
2018-05-13 12:53:25 -0300

    Comentado código do campo de busca

--- views/template.php
+++ views/template.php
@@ -21,7 +21,7 @@
 		<div class="topo">
 			<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
 				<a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">NSI - Controle de Entregas</a>
-				<input class="form-control form-control-dark w-100" type="text" placeholder="Buscar" aria-label="Search">
+				<!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Buscar" aria-label="Search"> -->
 				<ul class="navbar-nav px-3">
 					<li class="nav-item text-nowrap">
 						
